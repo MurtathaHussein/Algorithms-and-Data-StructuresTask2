66b5f437889091487a86cbbe8921534386a7ba97
murtatha hussein <103275443+MurtathaHussein@users.noreply.github.com>
2022-04-10 15:40:15 +0100

    Add files via upload

---
+++ ExcerciseWeek2TaskA/Classes/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciseWeek2TaskA
+{
+    class Student
+    {
+        // Class Variables / Members
+        private int age;
+        private String name;
+        private int Id;
+        
+        // Constructor - uisng only name as input 
+        public Student(String Aname, int Aage, int AId)
+        {
+            this.name = Aname;
+            this.age = Aage;
+            this.Id = AId;
+        }
+
+        //getter & setter for name 
+        public string Name
+        {
+            get { return name; }
+            set { this.name = value; }
+        }
+
+        //getter & setter for id 
+        public int ID
+        {
+            get { return Id; }
+            set { this.Id = value; }
+        }
+
+        //getter & setter for age 
+        public int Age
+        {
+            get { return age; }
+            set { this.age = value; }
+        }
+        
+        //method to return student information in string format
+        public string GetInfo()
+        {
+            return Id + ":"+ name + ":" + age;
+        }
+    }
+}
---
+++ ExcerciseWeek2TaskA/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseWeek2TaskA
+{
+ //Murtatha Hussein
+ // MMU ID: 21432516
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //Student array object to hold student info
+            Student[] student = new Student[10];
+            student[0] = new Student("Jade", 19, 112233); //creating student instance with its name,age and id and saving to student array
+            student[1] = new Student("Jack", 22, 223344);
+            student[2] = new Student("Mason", 20, 334455);
+            student[3] = new Student("Laila", 23, 556677);
+            student[4] = new Student("Yousef", 19, 667788);
+            student[5] = new Student("Mike", 19, 212131);
+            student[6] = new Student("Halima", 18, 202040);
+            student[7] = new Student("Mustafa", 26, 050695);
+            student[8] = new Student("Khadija", 22, 090999);
+            student[9] = new Student("Zahra", 22, 171098);
+
+            //to display student info from student array using loop
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(student[i].GetInfo());  //GetInfo method returns student information
+
+            Console.ReadKey();
+        }
+    }
+}
